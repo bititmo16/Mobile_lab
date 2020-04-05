fc6b4dd76ccc6684515640e74eabfdb4839cabb7
= <=>
2020-04-05 15:55:41 +0300

    first commit

---
+++ Lab1/bill/include/readCSV.h
@@ -0,0 +1,15 @@
+#ifndef READCSV_H
+#define READCSV_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+
+using namespace std;
+
+void readCSV(string num);
+void show_call(string num);
+void tariff(string num);
+#endif // READCSV_H
